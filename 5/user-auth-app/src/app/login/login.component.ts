1349e2be135e8119fcadfdd455ae0ebc21fdc8a0
kartikhalkude <kartikhalkude@gmail.com>
2025-05-06 01:03:57 +0530

    Initial commit

---
+++ 5/user-auth-app/src/app/login/login.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { UserService } from '../user.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private userService: UserService, private router: Router) { }
+
+  login(): void {
+    const success = this.userService.login(this.email, this.password);
+    if (success) {
+      this.router.navigate(['/profile']);
+    } else {
+      alert('Invalid Credentials!');
+    }
+  }
+}
