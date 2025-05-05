ff424dba195d97bfddb3acf1752d14e9bcad98a7
kartikhalkude <kartikhalkude@gmail.com>
2025-05-06 01:24:06 +0530

    Initial commit

---
+++ 5/user-auth-app-new/src/app/profile/profile.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink, Router } from '@angular/router';
+import { UserService } from '../../../../../user-auth-app-new/src/app/user.service';
+
+@Component({
+    selector: 'app-profile',
+    standalone: true,
+    imports: [CommonModule, RouterLink],
+    templateUrl: './profile.component.html',
+    styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+    user: any;
+
+    constructor(private userService: UserService, private router: Router) { }
+
+    ngOnInit() {
+        this.user = this.userService.getUser();
+        if (!this.user) {
+            this.router.navigate(['/login']);
+        }
+    }
+
+    logout() {
+        this.userService.logout();
+        this.router.navigate(['/login']);
+    }
+} 
